72517c19538d147d1a848b8a726dac46774cddd1
COrtega <carlos.ortega@sjsu.edu>
2023-10-19 14:00:59 -0700

    Added links for navigation using ul tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,34 @@
-
+import FB from '../../public/facebook.png'
+import Twitter from '../../public/twitter.png'
+import Insta from '../../public/instagram.png'
+import Linkedin from '../../public/linkedin.png'
 
 function Footer() {
 
-    // return (
-
-    // );
+    return (
+        <>
+            <img src='/public/Logo.svg'></img>
+            <ul title="Doormat Navigation">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#reservations">Reservations</a></li>
+                <li><a href="#order">Order Online</a></li>
+                <li><a href="#login">Login</a></li>
+            </ul>
+            <ul title="Contact Us">
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+            <ul title="Social Media Links">
+                <li><FB/></li>
+                <li><Twitter/></li>
+                <li><Insta/></li>
+                <li><Linkedin/></li>
+            </ul>
+        </>
+    );
 
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,18 @@
 
 function Nav() {
 
-    // return (
-
-    // );
+    return (
+        <>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#reservations">Reservations</a></li>
+                <li><a href="#order">Order Online</a></li>
+                <li><a href="#login">Login</a></li>
+            </ul>
+        </>
+    );
 
 }
 
