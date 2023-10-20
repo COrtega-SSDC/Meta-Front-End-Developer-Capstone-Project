98ae737b263afa799122921b41c937d5b7820438
COrtega <carlos.ortega@sjsu.edu>
2023-10-19 17:06:11 -0700

    changed name of logo.svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
 
     return (
         <>
-            <img src='./images/Logo.svg' alt="Little Lemon Logo"></img>
+            <img src='./images/logo.svg' alt="Little Lemon Logo"></img>
             <ul title="Doormat Navigation">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
