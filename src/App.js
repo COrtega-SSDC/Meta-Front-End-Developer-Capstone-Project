370fb627d500519baea2467d5be609339000f77a
COrtega <carlos.ortega@sjsu.edu>
2023-10-25 15:43:09 -0700

    added react router to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './css/App.css';
 // import { useRoutes } from 'react-router-dom';
 // import router from './Router';
 
+// import { Routes, Route } from 'react-router-dom';
+
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Homepage from './components/Homepage';
@@ -14,6 +16,10 @@ function App() {
       <Nav/>
       <Homepage/>
       <Footer/>
+      {/* <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/bro' element={<Footer/>}></Route>
+      </Routes> */}
     </div>
   );
 }
