7ef2adb19440331740ea90bcdd15dbdd42cfe7db
COrtega <carlos.ortega@sjsu.edu>
2023-10-25 14:36:17 -0700

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Header from './components/Header'
 import Nav from './components/Nav'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Homepage from './components/Homepage';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Header/>
       <Nav/>
+      <Homepage/>
       <Main/>
       <Footer/>
     </div>
