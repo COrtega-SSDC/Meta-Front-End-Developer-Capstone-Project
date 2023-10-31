8400cdfaea910d3690576165b6c57815169f6c27
COrtega <carlos.ortega@sjsu.edu>
2023-10-31 12:41:34 -0700

    changed <a> to Link and added CSS styling

--- src/components/Semantic Elements/Nav.js
+++ src/components/Semantic Elements/Nav.js
@@ -1,17 +1,23 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Nav() {
 
+    const style = {
+        fontSize: 18,
+        fontFamily: 'Karla',
+        fontWeight: '800',
+        textTransform: 'capitalize',
+        wordWrap: 'break-word'
+    }
+
     return (
-        <nav>
-            <ul>
-                <Link to="/" >Home</Link>
-                <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/reservations">Reservations</a></li>
-                <li><a href="/order">Order Online</a></li>
-                <li><a href="/login">Login</a></li>
-            </ul>
+        <nav className='nav'>
+            <Link to="/" style={style}>Home</Link>
+            <Link to="/about" style={style}>About</Link>
+            <Link to="/menu" style={style}>Menu</Link>
+            <Link to="/reservations" style={style}>Reservations</Link>
+            <Link to="/order" style={style}>Order Online</Link>
+            <Link to="/login" style={style}>Login</Link>
         </nav>
     );
 
