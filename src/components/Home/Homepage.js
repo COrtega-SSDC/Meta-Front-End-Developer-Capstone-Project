fab7306880e7b3f259620956ea2861514f9ad826
COrtega <carlos.ortega@sjsu.edu>
2023-10-31 15:07:18 -0700

    fixed issue of duplicate header and footer

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -3,19 +3,14 @@ import Specials from './Specials';
 import Testimonials from './Testimonials';
 import Backstory from './Backstory';
 
-import Header from '../Semantic Elements/Header';
-import Footer from '../Semantic Elements/Footer';
-
 function Homepage() {
 
     return (
         <div className='Homepage'>
-            <Header/>
             <Hero />
             <Specials />
             <Testimonials/>
             <Backstory/>
-            <Footer/>
         </div>
 
     );
