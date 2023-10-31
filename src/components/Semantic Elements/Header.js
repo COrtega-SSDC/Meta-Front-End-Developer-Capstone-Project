22e1c452c77115a9e28e0b99c5a25e20302c9b32
COrtega <carlos.ortega@sjsu.edu>
2023-10-31 15:09:42 -0700

    completed header. need only spacing to fix

--- src/components/Semantic Elements/Header.js
+++ src/components/Semantic Elements/Header.js
@@ -1,11 +1,29 @@
 
 import Nav from "../Semantic Elements/Nav";
+
+import logo from '../../images/logo.png'
+import { HStack, Flex, Box, Container } from '@chakra-ui/react'
+
+
 function Header() {
+
+    // const style = {
+    //     width: '905px',
+    //     height: '78px',
+    //     flexShrink: 0
+    // };
 
     return (
         <>
-            <img src="src\images\logo.png" />
-            <Nav />
+            <Flex justifyContent="center" alignItems="center">
+                <HStack spacing='63px'>
+                    <Container maxWidth="202px" maxHeight="78px" flexShrink={0}>
+                        <img src={logo} alt="Little Lemon Logo"/>
+                    </Container>                    
+                    <Nav />
+                </HStack>
+            </Flex>
+
         </>
     );
 
