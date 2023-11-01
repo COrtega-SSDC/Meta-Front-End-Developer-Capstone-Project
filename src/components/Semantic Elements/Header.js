649c7bdcdd01605daeba1a6ee547d4ba56e66239
COrtega <carlos.ortega@sjsu.edu>
2023-11-01 15:58:12 -0700

    finalized logo style in header

--- src/components/Semantic Elements/Header.js
+++ src/components/Semantic Elements/Header.js
@@ -2,24 +2,37 @@
 import Nav from "../Semantic Elements/Nav";
 
 import logo from '../../images/logo.png'
-import { HStack, Flex, Container } from '@chakra-ui/react'
+import { HStack, Flex, Box } from '@chakra-ui/react'
 
 
 function Header() {
 
-    // const style = {
-    //     width: '905px',
-    //     height: '78px',
-    //     flexShrink: 0
-    // };
+    const style = {
+        width: '202px',
+        height: '76px',
+        flexShrink: 0
+    };
+
+    const flex_style = {
+        justifyContent: "center",
+        paddingBottom: 26,
+        paddingTop: 21,
+        width: "905px",
+        height: "78px",
+        margin: "auto"
+    }
 
     return (
         <>
-            <Flex justifyContent="center" alignItems="center" paddingBottom={26} paddingTop={21}>
-                <HStack spacing='25px'>
-                    <Container maxWidth="202px" maxHeight="78px" flexShrink={0}>
-                        <img src={logo} alt="Little Lemon Logo"/>
-                    </Container>                    
+            <Flex style={flex_style}>
+                <HStack spacing='63px'>
+                    <Box as="img" 
+                        src={logo} 
+                        alt="Little Lemon Logo"
+                        objectPosition="center"
+                        objectFit="contain"
+                        style={style}
+                    />                    
                     <Nav />
                 </HStack>
             </Flex>
