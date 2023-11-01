62b4288bb6c46a7fe32019abdd66df20b638c936
COrtega <carlos.ortega@sjsu.edu>
2023-10-31 17:25:16 -0700

    added links from router and formatting

--- src/components/Semantic Elements/Footer.js
+++ src/components/Semantic Elements/Footer.js
@@ -3,35 +3,61 @@ import Twitter from '../../images/twitter.png'
 import Insta from '../../images/instagram.png'
 import LinkedIn from '../../images/linkedin.png'
 
-import logo from '../../images/logo.png'
+import logo from '../../images/alt-logo.png'
+
+import { HStack, Flex, Container, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
 
 function Footer() {
 
+    const style = {
+        color: '#EDEFEE',
+        fontSize: 18,
+        fontFamily: 'Karla',
+        fontWeight: '500',
+        // lineHeight: 23.40,
+        wordWrap: 'break-word'
+    }
+
     return (
         <>
-            <img src={logo} alt="Little Lemon Logo"/>
-            <h3>Doormat Navigation</h3>
-            <ul title="Doormat Navigation">
-                <li><a href="/home">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/reservations">Reservations</a></li>
-                <li><a href="/order">Order Online</a></li>
-                <li><a href="/login">Login</a></li>
-            </ul>
-            <h3>Contact Us</h3>
-            <ul title="Contact Us">
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
-            </ul>
-            <h3>Social Media Links</h3>
-            <ul title="Social Media Links">
-                <li><img src={Facebook} alt="Little Lemon Logo"/></li>
-                <li><img src={Twitter} alt="Little Lemon Logo"/></li>
-                <li><img src={Insta} alt="Little Lemon Logo"/></li>
-                <li><img src={LinkedIn} alt="Little Lemon Logo"/></li>
-            </ul>
+                <HStack bg='#333333' justifyContent="center" paddingY="95px">
+                    <img src={logo} alt="Alternate Little Lemon Logo" />
+                    <VStack align="flex-start">
+                        <h3>Doormat Navigation</h3>
+                        <Link to="/" style={style}>Home</Link>
+                        <Link to="/about" style={style}>About</Link>
+                        <Link to="/menu" style={style}>Menu</Link>
+                        <Link to="/reservations" style={style}>Reservations</Link>
+                        <Link to="/order" style={style}>Order Online</Link>
+                        <Link to="/login" style={style}>Login</Link>
+                    </VStack>
+                    <VStack align="flex-start">
+                        <h3>Contact Us</h3>
+                        <p style={style}>Address: </p>
+                        <p style={style}>1100 Hyde Park, Chicago, IL, 60615</p>
+                        <p style={style}>Phone #: </p>
+                        <p style={style}>+1 (312) 555- 0142</p>
+                        <p style={style}>Email: </p>
+                        <p style={style}>info@littlelemon.com</p>
+                    </VStack>
+                    <VStack align="flex-start">
+                        <h3>Social Media Links</h3>
+                        <ul title="Social Media Links">
+                            <a href='https://www.facebook.com/LittleLemon' style={style} target="_blank" rel="noopener noreferrer">Facebook</a>
+                            <li><img src={Facebook} alt="Little Lemon Logo" width="24px" height="24px" /></li>
+                            <a href='https://www.twitter.com/LittleLemon' style={style} target="_blank" rel="noopener noreferrer">Twitter</a>
+                            <li><img src={Twitter} alt="Little Lemon Logo" width="24px" height="24px" /></li>
+                            <a href='https://www.instagram.com/LittleLemon' style={style} target="_blank" rel="noopener noreferrer">Instagram</a>
+                            <li><img src={Insta} alt="Little Lemon Logo" width="24px" height="24px" /></li>
+                            <a href='https://www.linkedin.com/LittleLemon' style={style} target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                            <li><img src={LinkedIn} alt="Little Lemon Logo" width="24px" height="24px" /></li>
+                            <a href='https://www.tiktok.com/LittleLemon' style={style} target="_blank" rel="noopener noreferrer">TikTok</a>
+                        </ul>
+                    </VStack>
+                </HStack>
+
         </>
     );
 
