4adf6c2a7f93ec81fb9733c58d1c1032685ac970
COrtega <carlos.ortega@sjsu.edu>
2023-11-01 16:21:49 -0700

    added react fragment

--- src/components/Semantic Elements/Main.js
+++ src/components/Semantic Elements/Main.js
@@ -10,14 +10,16 @@ import BookingPage from '../Reservations/Booking Page'
 function Main() {
 
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/about" element={<BookingPage />}></Route>
-            <Route path="/menu" element={<BookingPage />}></Route>
-            <Route path="/reservations" element={<BookingPage />}></Route>
-            <Route path="/order online" element={<BookingPage />}></Route>
-            <Route path="/login" element={<BookingPage />}></Route>
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path="/about" element={<BookingPage />}></Route>
+                <Route path="/menu" element={<BookingPage />}></Route>
+                <Route path="/reservations" element={<BookingPage />}></Route>
+                <Route path="/order online" element={<BookingPage />}></Route>
+                <Route path="/login" element={<BookingPage />}></Route>
+            </Routes>
+        </>
     );
 
 }
