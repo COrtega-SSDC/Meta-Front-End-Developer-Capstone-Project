430bd1ad3f0631dcb5d367be26cb677b419c1470
COrtega <carlos.ortega@sjsu.edu>
2023-11-02 15:48:29 -0700

    added the skeleton for testimonials

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,10 +1,39 @@
-
+import { Box, Flex, HStack, Heading, VStack } from '@chakra-ui/react'
 
 function Testimonials() {
 
-    // return (
 
-    // );
+    const head = {
+        fontFamily: 'Markazi Text',
+        fontSize: 64,
+        fontWeight: 400,
+        wordWrap: 'break-word'
+    }
+    return (
+        <Flex bg="#EDEFEE" height="590px" justifyContent="center">
+            <VStack spacing="77px">
+                <Heading style={head} paddingTop="80px">
+                    Testimonials
+                </Heading>
+                <HStack spacing="20px">
+                    <Box width="200px" height="175px" bg="#FFFFFF">
+
+                    </Box>
+                    <Box width="200px" height="175px" bg="#FFFFFF">
+
+                    </Box>
+                    <Box width="200px" height="175px" bg="#FFFFFF">
+
+                    </Box>
+                    <Box width="200px" height="175px" bg="#FFFFFF">
+
+                    </Box>
+                </HStack>
+            </VStack>
+
+
+        </Flex>
+    );
 
 }
 
