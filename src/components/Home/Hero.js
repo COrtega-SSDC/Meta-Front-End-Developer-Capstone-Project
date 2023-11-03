ddd8936fe2048bfff175529b72290ee2df98113d
COrtega <carlos.ortega@sjsu.edu>
2023-11-03 11:43:03 -0700

    added link to button and finalized styling

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -2,6 +2,7 @@
 import { Button, HStack, VStack, Box, Flex, Spacer } from '@chakra-ui/react'
 
 import img from '../../images/restauranfood.jpg'
+import { Link } from 'react-router-dom'
 
 function Hero() {
 
@@ -20,14 +21,14 @@ function Hero() {
         <>
             <Flex justifyContent="center" bg='#495E57' height="406px" marginTop="26px" >
                 <HStack spacing='239px'>
-                    <VStack width="291px" height="355px">
-                        <Box height="106px" width="280px" alignContent="flex-start">
+                    <VStack width="291px" height="355px" align="flex-start">
+                        <Box height="106px" width="280px">
                             <Box as='h1' >Little Lemon</Box>
-                            <Box as='h2' >Chicago</Box>
+                            <Box as='h2' textAlign="left" mt="-20px" >Chicago</Box>
                         </Box>
                         <Spacer />
                         <Box w='291px' h='132px'>
-                            <Box as='h4' style={{lineHeight: '28.125px'}}>
+                            <Box as='h4' style={{ lineHeight: '25.31px', textAlign: "left" }}>
                                 We are a family owned
                                 Mediterranean restaurant,
                                 focused on traditional recipes served with a modern
@@ -35,12 +36,14 @@ function Hero() {
                             </Box>
                         </Box>
                         <Spacer />
-                        <Button
-                            bgColor="#F4CE14"
-                            style={button_style}
-                        >
-                            Reserve a Table
-                        </Button>
+                        <Link to="/reservations">
+                            <Button
+                                bgColor="#F4CE14"
+                                style={button_style}
+                            >
+                                Reserve a Table
+                            </Button>
+                        </Link>
                     </VStack>
                     <Box
                         as='img'
