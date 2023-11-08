10d215bcd0d8adc3de76267146d4c7039935bc09
COrtega <carlos.ortega@sjsu.edu>
2023-11-08 15:43:16 -0800

    added Confimred Booking page

---
+++ src/components/Reservations/Confirmed Booking.js
@@ -0,0 +1,14 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+function ConfirmedBooking () {
+    return (
+        <>
+            <Flex>
+                <Heading as='h1'>Reservation confirmed</Heading>
+            </Flex>
+        </>
+    )
+
+}
+
+export default ConfirmedBooking;
