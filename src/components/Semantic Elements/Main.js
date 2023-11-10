951af24356f81f49a6722f5d6e8f49523df19120
COrtega <carlos.ortega@sjsu.edu>
2023-11-09 17:39:42 -0800

    put useEffect code into initializeTimes moved initializeTimes and updateTimes out to export for test added navigation to confirmation and routes

--- src/components/Semantic Elements/Main.js
+++ src/components/Semantic Elements/Main.js
@@ -2,9 +2,9 @@
 // import { useRoutes } from 'react-router-dom';
 // import router from './Router';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useEffect, useReducer } from 'react';
-import { fetchAPI } from '../../api';
+import { fetchAPI, submitAPI } from '../../api';
 
 import HomePage from '../Home/Homepage';
 import BookingPage from '../Reservations/Booking Page'
@@ -12,56 +12,24 @@ import ConfirmedBooking from '../Reservations/Confirmed Booking';
 
 function Main() {
 
-  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+  const [availableTimes, dispatch] = useReducer(updateTimes, []);
+  const navigate = useNavigate(); // Hook for navigation
 
 
-  function initializeTimes() {
-    return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
-  }
+  // function initializeTimes() {
+  //   return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
+  // }
 
-  //   function updateTimes(state, action) {
-  //     switch (action.type) {
-  //       case 'SET_TIMES':
-  //         return action.payload;
-  //       case 'UPDATE_TIMES':
-  //         window.fetchAPI(new Date(action.payload))
-  //           .then(times => {
-  //             dispatch({ type: 'SET_TIMES', payload: times });
-  //           })
-  //           .catch(error => {
-  //             console.error('Failed to fetch times: ', error);
-  //           });
-  //         // Important: Don't change the state yet, it will be changed by the SET_TIMES action.
-  //         return state;
-  //       default:
-  //         return state;
-  //     }
-  //   }
-
-  function updateTimes(state, action) {
-    switch (action.type) {
-      case 'SET_TIMES':
-        return action.payload;
-      default:
-        return state;
+  const submitForm = formData => {
+    const submissionResult = submitAPI(formData);
+    if (submissionResult) {
+      navigate('/confirmation'); // Navigate to the booking confirmed page
     }
-  }
-
-  // useEffect(() => {
-  //     initializeTimes();
-  //   }, []);
+  };
 
   useEffect(() => {
-    const today = new Date();
-    const formattedDate = ((today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear());
-    fetchAPI(formattedDate)
-      .then(times => {
-        dispatch({ type: 'SET_TIMES', payload: times });
-      })
-      .catch(error => {
-        console.error('Failed to fetch times:', error);
-      });
-  }, []);
+      initializeTimes(dispatch);
+    }, [dispatch]);
 
   return (
     <>
@@ -69,13 +37,36 @@ function Main() {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<BookingPage />}></Route>
         <Route path="/menu" element={<BookingPage />}></Route>
-        <Route path="/reservations" element={<BookingPage availableTimes={availableTimes} dispatch={dispatch} />}></Route>
-        <Route path="/order" element={<ConfirmedBooking />}></Route>
+        <Route path="/reservations" element={<BookingPage availableTimes={availableTimes} dispatch={dispatch} submitForm={submitForm}/>}></Route>
+        <Route path="/order" element={<BookingPage />}></Route>
         <Route path="/login" element={<BookingPage />}></Route>
+        <Route path="/confirmation" element={<ConfirmedBooking />}></Route>
       </Routes>
     </>
   );
 
+}
+
+export function initializeTimes(dispatch) {
+  const today = new Date();
+  const formattedDate = ((today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear());
+  
+  fetchAPI(formattedDate)
+    .then(times => {
+      dispatch({ type: 'SET_TIMES', payload: times });
+    })
+    .catch(error => {
+      console.error('Failed to fetch times:', error);
+    });
+};
+
+export function updateTimes(state, action) {
+  switch (action.type) {
+    case 'SET_TIMES':
+      return action.payload;
+    default:
+      return state;
+  }
 }
 
 export default Main;
