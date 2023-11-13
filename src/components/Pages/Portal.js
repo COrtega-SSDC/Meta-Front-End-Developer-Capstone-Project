f9fb2274e769f8680d1c77604ca7b505fad6fb8b
COrtega <carlos.ortega@sjsu.edu>
2023-11-13 15:35:09 -0800

    added spacing

--- src/components/Pages/Portal.js
+++ src/components/Pages/Portal.js
@@ -4,7 +4,7 @@ function Portal() {
     return (
         <>
             <Flex justifyContent="center" mb="100px" alignItems="center">
-                <VStack>
+                <VStack spacing={10}>
                     <Heading as='h1' color="black">Under Maintenance</Heading>
                     <Text>We are working on adding this feature in the near future!</Text>
                 </VStack>
